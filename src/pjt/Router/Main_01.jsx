a5c5ea5d8fd964ab2cd0af6a167b49b86390b7e6
Ahnjisoo <wltn24996@gmail.com>
2023-06-13 10:50:21 +0900

    new repository first commit

---
+++ src/pjt/Router/Main_01.jsx
@@ -0,0 +1,97 @@
+import { useState, useEffect, useRef } from 'react';
+
+import Dots from './Dots';
+
+import './css/main.css';
+
+const DIVIDER_HEIGHT = 5;
+
+function App() {
+    const outerDivRef = useRef();
+    const [scrollIndex, setScrollIndex] = useState(1);
+    useEffect(() => {
+        const wheelHandler = (e) => {
+            e.preventDefault();
+            const { deltaY } = e;
+            const { scrollTop } = outerDivRef.current; // 스크롤 위쪽 끝부분 위치
+            const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
+
+            if (deltaY > 0) {
+                // 스크롤 내릴 때
+                if (scrollTop >= 0 && scrollTop < pageHeight) {
+                    //현재 1페이지
+                    console.log('현재 1페이지, down');
+                    outerDivRef.current.scrollTo({
+                        top: pageHeight + DIVIDER_HEIGHT,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setScrollIndex(2);
+                } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+                    //현재 2페이지
+                    console.log('현재 2페이지, down');
+                    outerDivRef.current.scrollTo({
+                        top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setScrollIndex(3);
+                } else {
+                    // 현재 3페이지
+                    console.log('현재 3페이지, down');
+                    outerDivRef.current.scrollTo({
+                        top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setScrollIndex(3);
+                }
+            } else {
+                // 스크롤 올릴 때
+                if (scrollTop >= 0 && scrollTop < pageHeight) {
+                    //현재 1페이지
+                    console.log('현재 1페이지, up');
+                    outerDivRef.current.scrollTo({
+                        top: 0,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setScrollIndex(1);
+                } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+                    //현재 2페이지
+                    console.log('현재 2페이지, up');
+                    outerDivRef.current.scrollTo({
+                        top: 0,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setScrollIndex(1);
+                } else {
+                    // 현재 3페이지
+                    console.log('현재 3페이지, up');
+                    outerDivRef.current.scrollTo({
+                        top: pageHeight + DIVIDER_HEIGHT,
+                        left: 0,
+                        behavior: 'smooth',
+                    });
+                    setScrollIndex(2);
+                }
+            }
+        };
+        const outerDivRefCurrent = outerDivRef.current;
+        outerDivRefCurrent.addEventListener('wheel', wheelHandler);
+        return () => {
+            outerDivRefCurrent.removeEventListener('wheel', wheelHandler);
+        };
+    }, []);
+    return (
+        <div ref={outerDivRef} className="outer">
+            <Dots scrollIndex={scrollIndex} />
+            <div className="inner bg-yellow"></div>
+            <div className="inner bg-blue"></div>
+            <div className="inner bg-pink"></div>
+        </div>
+    );
+}
+
+export default App;
