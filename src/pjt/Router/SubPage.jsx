c158a13704ec9112cfda3eb373ff6bc493d2d3a3
Ahnjisoo <wltn24996@gmail.com>
2023-06-14 10:37:02 +0900

    ONETEAMBTC-31 컴포넌트 구조 분할 완료

--- src/pjt/Router/SubPage.jsx
+++ src/pjt/Router/SubPage.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React from 'react';
+import CityInfo from './subpage/CityInfo';
+import MapInfo from './subpage/MapInfo';
+import TrafficInfo from './subpage/TrafficInfo';
+import WeatherInfo from './subpage/WeatherInfo';
 
 const SubPage = () => {
-    return(
+    return (
         <>
+            <CityInfo />
+            <MapInfo />
+            <TrafficInfo />
+            <WeatherInfo />
         </>
     );
-}
+};
 
 export default SubPage;
