8fc5b912b7a17688017eef8e66290017aea7fe62
Ahnjisoo <wltn24996@gmail.com>
2023-06-14 16:38:16 +0900

    MapInfo 제작 2번

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
                 </Routes>
                 <Footer />
             </BrowserRouter>
-            <TmapTrans />
+            {/* <TmapTrans /> */}
         </>
     );
 }
