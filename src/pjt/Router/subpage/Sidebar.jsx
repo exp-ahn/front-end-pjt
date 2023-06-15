613bc705ee487c3e7ec2dd21e8d3d01cb95df45f
doradorani <sokoyo2@gmail.com>
2023-06-15 17:01:15 +0900

    ONETEAM-46 필터 지역부분 단어 하나만 저장

--- src/pjt/Router/subpage/Sidebar.jsx
+++ src/pjt/Router/subpage/Sidebar.jsx
@@ -1,43 +1,43 @@
-import React, { useEffect } from "react";
-import "../css/common.css";
-import "../css/style.css";
+import React, { useEffect } from 'react';
+import '../css/common.css';
+import '../css/style.css';
 
 const AREA_LIST_1 = [
-    { id: "서울", area: "서울" },
-    { id: "부산", area: "부산" },
-    { id: "광주", area: "광주" },
-    { id: "울산", area: "울산" },
+    { id: '서울', area: '서울' },
+    { id: '부산', area: '부산' },
+    { id: '광주', area: '광주' },
+    { id: '울산', area: '울산' },
 ];
 const AREA_LIST_2 = [
-    { id: "대전", area: "대전" },
-    { id: "대구", area: "대구" },
-    { id: "인천", area: "인천" },
-    { id: "제주", area: "제주" },
+    { id: '대전', area: '대전' },
+    { id: '대구', area: '대구' },
+    { id: '인천', area: '인천' },
+    { id: '제주', area: '제주' },
 ];
 const TOUR_1 = [
-    { id: "테마파크", tour: "테마파크" },
-    { id: "박물관", tour: "박물관" },
+    { id: '테마파크', tour: '테마파크' },
+    { id: '박물관', tour: '박물관' },
 ];
 const RESTAURANT_1 = [
-    { id: "한식", food: "한식" },
-    { id: "양식", food: "양식" },
-    { id: "중식", food: "중식" },
+    { id: '한식', food: '한식' },
+    { id: '양식', food: '양식' },
+    { id: '중식', food: '중식' },
 ];
 
 const SideBar_test = ({ checkedArea, setCheckedArea, checkedTour, setCheckedTour }) => {
     useEffect(() => {
-        console.log("체크박스 변동!!!");
+        console.log('체크박스 변동!!!');
     }, [checkedArea]);
 
     const onCheckedRadio = (checked, item) => {
         //라디오용
         if (checked) {
-            console.log(item, "체크됨");
+            console.log(item, '체크됨');
             setCheckedArea(item);
 
             console.log(checkedArea); // 리스트 테스트용
         } else if (!checked) {
-            console.log(item, "체크 헤제됨");
+            console.log(item, '체크 헤제됨');
 
             setCheckedArea(checkedArea.filter((el) => el !== item)); //체크박스 전용
 
@@ -47,7 +47,7 @@ const SideBar_test = ({ checkedArea, setCheckedArea, checkedTour, setCheckedTour
     const onCheckedCheckBox = (checked, item) => {
         //체크박스용
         if (checked) {
-            console.log(item, "체크됨");
+            console.log(item, '체크됨');
             setCheckedTour([...checkedTour, item]);
 
             // checkedTour.push(item);
@@ -56,7 +56,7 @@ const SideBar_test = ({ checkedArea, setCheckedArea, checkedTour, setCheckedTour
 
             console.log(checkedTour); // 리스트 테스트용
         } else if (!checked) {
-            console.log(item, "체크 헤제됨");
+            console.log(item, '체크 헤제됨');
 
             setCheckedTour(checkedTour.filter((el) => el !== item)); //체크박스 전용
 
@@ -65,17 +65,17 @@ const SideBar_test = ({ checkedArea, setCheckedArea, checkedTour, setCheckedTour
     };
 
     return (
-        <div className='sidebar'>
-            <div className='filter-head'>필터</div>
-            <div className='filter-category'>
+        <div className="sidebar">
+            <div className="filter-head">필터</div>
+            <div className="filter-category">
                 <p>지역</p>
-                <ul className='filter-checkbox'>
+                <ul className="filter-checkbox">
                     {AREA_LIST_1.map((item) => {
                         return (
                             <li key={item.id}>
                                 <input
-                                    type='radio'
-                                    name='area_radio'
+                                    type="radio"
+                                    name="area_radio"
                                     value={item.area}
                                     onChange={(e) => {
                                         onCheckedRadio(e.target.checked, e.target.value);
@@ -86,16 +86,16 @@ const SideBar_test = ({ checkedArea, setCheckedArea, checkedTour, setCheckedTour
                         );
                     })}
                 </ul>
-                <ul className='filter-checkbox'>
+                <ul className="filter-checkbox">
                     {AREA_LIST_2.map((item) => {
                         return (
                             <li key={item.id}>
                                 <input
-                                    type='radio'
-                                    name='area_radio'
+                                    type="radio"
+                                    name="area_radio"
                                     value={item.area}
                                     onChange={(e) => {
-                                        onCheckedCheckBox(e.target.checked, e.target.value);
+                                        onCheckedRadio(e.target.checked, e.target.value);
                                     }}
                                 />
                                 {item.area}
@@ -105,14 +105,14 @@ const SideBar_test = ({ checkedArea, setCheckedArea, checkedTour, setCheckedTour
                 </ul>
             </div>
 
-            <div className='filter-category'>
+            <div className="filter-category">
                 <p>관광지</p>
-                <ul className='filter-checkbox'>
+                <ul className="filter-checkbox">
                     {TOUR_1.map((item) => {
                         return (
                             <li key={item.id}>
                                 <input
-                                    type='checkbox'
+                                    type="checkbox"
                                     value={item.tour}
                                     onChange={(e) => {
                                         onCheckedCheckBox(e.target.checked, e.target.value);
@@ -125,14 +125,14 @@ const SideBar_test = ({ checkedArea, setCheckedArea, checkedTour, setCheckedTour
                 </ul>
             </div>
 
-            <div className='filter-category'>
+            <div className="filter-category">
                 <p>맛집</p>
-                <ul className='filter-checkbox'>
+                <ul className="filter-checkbox">
                     {RESTAURANT_1.map((item) => {
                         return (
                             <li key={item.id}>
                                 <input
-                                    type='checkbox'
+                                    type="checkbox"
                                     value={item.food}
                                     onChange={(e) => {
                                         onCheckedCheckBox(e.target.checked, e.target.value);
