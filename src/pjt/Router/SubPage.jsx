99139c05c63a7a1d2d6236d24636753e1bd5f3b7
doradorani <sokoyo2@gmail.com>
2023-06-15 17:21:58 +0900

    ONETEAMbtc-48 필터에서 지역값받음 & 영어로 변환해서 API 호출 & 날씨 XHTML 뼈대

--- src/pjt/Router/SubPage.jsx
+++ src/pjt/Router/SubPage.jsx
@@ -1,27 +1,27 @@
-import React, { useEffect, useState } from "react";
-import CityInfo from "./subpage/CityInfo";
-import MapInfo from "./subpage/MapInfo";
-import TrafficInfo from "./subpage/TrafficInfo";
-import WeatherInfo from "./subpage/WeatherInfo";
-import Sidebar from "./subpage/Sidebar";
-
+import React, { useEffect, useState } from 'react';
+import CityInfo from './subpage/CityInfo';
+import MapInfo from './subpage/MapInfo';
+import TrafficInfo from './subpage/TrafficInfo';
+import WeatherInfo from './subpage/WeatherInfo';
+import Sidebar from './subpage/Sidebar';
+import './css/subPage.css';
 const SubPage = () => {
     //SideBar에서 관리 START
-    const [checkedArea, setCheckedArea] = useState([]);
+    const [checkedArea, setCheckedArea] = useState('');
     const [checkedTour, setCheckedTour] = useState([]);
     //SideBar에서 관리 END
 
     //SideBar에서 온 훅으로 Keyword 생성 START
     const keyword = checkedArea + checkedTour;
-    console.log("keyword ---> ", keyword);
+    console.log('keyword ---> ', keyword);
     //SideBar에서 온 훅으로 Keyword 생성 END
 
     useEffect(() => {
-        console.log("[SubPage] 변경 사항 발생!!!");
+        console.log('[SubPage] 변경 사항 발생!!!');
     });
 
     return (
-        <>
+        <div className="sub-wrap">
             <div>
                 <Sidebar
                     checkedArea={checkedArea}
@@ -36,9 +36,9 @@ const SubPage = () => {
             </div>
             <div>
                 <TrafficInfo />
-                <WeatherInfo />
+                <WeatherInfo checkedArea={checkedArea} />
             </div>
-        </>
+        </div>
     );
 };
 
