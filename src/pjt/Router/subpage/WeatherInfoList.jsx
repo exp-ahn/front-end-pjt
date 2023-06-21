ce73ae6e85815971046a6155ee7cce80fdaabf5a
doradorani <sokoyo2@gmail.com>
2023-06-21 14:58:12 +0900

    ONETEAMBTC-61 현 위치 출발지 기능 추가

--- src/pjt/Router/subpage/WeatherInfoList.jsx
+++ src/pjt/Router/subpage/WeatherInfoList.jsx
@@ -4,8 +4,8 @@ const weatherDescriptionData = {
     맑음: 'Clear',
     흐림: 'Clouds',
     비: 'Rain',
-    '약간 비': 'Drizzle',
-    '천둥 번개': 'Thunderstorm',
+    가랑비: 'Drizzle',
+    '천둥/번개': 'Thunderstorm',
     눈: 'Snow',
     안개: 'Mist',
     황사: 'Dust',
@@ -25,11 +25,11 @@ const weatherDayData = {
 const WeatherInfoList = ({ month, date, day, hour, icon, main, feels_like, temp }) => {
     return (
         <tr className="weather-forecast-value">
-            <td>{`${month}. ${date}.${Object.keys(weatherDayData).find(
+            <td>{`${month}.${date}.${Object.keys(weatherDayData).find(
                 (key) => weatherDayData[key] === day
-            )}. ${hour}:00`}</td>
+            )} / ${hour}시`}</td>
             <td>
-                <img className="weather-icon" src={`./weather_icon/${icon}.png`} alt="Weather Icon" />
+                <img className="weather-forecast-value-icon" src={`./weather_icon/${icon}.png`} alt="Weather Icon" />
             </td>
             <td>{Object.keys(weatherDescriptionData).find((key) => weatherDescriptionData[key] === main)}</td>
             <td>{`${parseInt(temp - 273.15)}°C`}</td>
